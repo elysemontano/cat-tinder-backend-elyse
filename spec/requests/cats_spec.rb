b9e91ae163f9b8e071b52cbbd7ad0ed4718f3cda
Elyse Montano <elysemontano.dev.com>
2022-10-18 16:19:38 -0700

    adds validations and specs

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Cats", type: :request do
+
+# ---------------------- CRUD
   describe "GET /index" do
     it "gets a list of cats" do
       Cat.create(
@@ -10,9 +12,7 @@ RSpec.describe "Cats", type: :request do
         image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
       )
 
-      # Make a request
       get '/cats'
-
       cat = JSON.parse(response.body)
       expect(response).to have_http_status(200)
       expect(cat.length).to eq 1
@@ -21,7 +21,6 @@ RSpec.describe "Cats", type: :request do
 
   describe "POST /create" do
     it "creates a cat" do
-      # The params we are going to send with the request
       cat_params = {
         cat: {
           name: 'Buster',
@@ -30,24 +29,15 @@ RSpec.describe "Cats", type: :request do
           image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
         }
       }
-  
-      # Send the request to the server
       post '/cats', params: cat_params
-  
-      # Assure that we get a success back
       expect(response).to have_http_status(200)
-  
-      # Look up the cat we expect to be created in the db
       cat = Cat.first
-  
-      # Assure that the created cat has the correct attributes
       expect(cat.name).to eq 'Buster'
     end
   end
 
   describe "PATCH /update" do
     it "updates a cat" do
-      # The params we are going to send with the request
       cat_params = {
         cat: {
           name: 'Buster',
@@ -56,8 +46,6 @@ RSpec.describe "Cats", type: :request do
           image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
         }
       }
-  
-      # Send the request to the server
       post '/cats', params: cat_params
       cat = Cat.first
 
@@ -72,16 +60,13 @@ RSpec.describe "Cats", type: :request do
   
       patch "/cats/#{cat.id}", params: updated_cat_params
       updated_cat = Cat.find(cat.id)
-      # Assure that we get a success back
       expect(response).to have_http_status(200)
-      # Assure that the updated cat has the correct attributes
       expect(updated_cat.age).to eq 5
     end
   end
 
   describe "DELETE /destroy" do
     it 'deletes a cat' do
-      # create the cat
       cat_params = {
         cat: {
           name: 'Buster',
@@ -96,6 +81,207 @@ RSpec.describe "Cats", type: :request do
       expect(response).to have_http_status(200)
       cats = Cat.all
       expect(cats).to be_empty
+    end
+  end
+
+
+
+
+
+#------------------ VALIDATIONS
+
+  describe "cannot create a cat without valid attributes" do
+    it "doesn't create a cat without a name" do
+      cat_params = {
+          cat: {
+          age: 5,
+          enjoys: 'snuggles and teasing the dog',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+          }
+      }
+      post '/cats', params: cat_params
+      expect(response.status).to eq 422
+      cat = JSON.parse(response.body)
+      expect(cat['name']).to include "can't be blank"
+    end
+
+    it 'cannot create a cat without an age' do
+      cat_params = {
+        cat: {
+          name: 'Boo',
+          enjoys: 'cuddles and belly rubs', 
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(cat['age']).to include "can't be blank"
+    end
+    it 'cannot create a cat without an enjoys' do
+      cat_params = {
+        cat: {
+          name: 'Boo',
+          age: 2, 
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(cat['enjoys']).to include "can't be blank"
+    end
+
+    it 'cannot create a cat without an image' do
+      cat_params = {
+        cat: {
+          name: 'Boo',
+          age: 2, 
+          enjoys: 'cuddles and belly rubs'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(cat['image']).to include "can't be blank"
+    end
+    it 'cannot create a cat without an enjoys that is at least 10 characters' do
+      cat_params = {
+        cat: {
+          name: 'Boo',
+          age: 2,
+          enjoys: 'cuddles', 
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(cat['enjoys']).to include "is too short (minimum is 10 characters)"
+    end
+  end
+
+  describe "cannot update a cat without valid attributes" do
+    it 'cannot update a cat without a name' do
+      cat_params = {
+        cat: {
+          name: 'Boo',
+          age: 2,
+          enjoys: 'cuddles and belly rubs',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = Cat.first
+      updated_cat_params = {
+        cat: {
+          name: '',
+          age: 2,
+          enjoys: 'cuddles and belly rubs', 
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      patch "/cats/#{cat.id}", params: updated_cat_params
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(cat['name']).to include "can't be blank"
+    end
+    it 'cannot update a cat without a age' do
+      cat_params = {
+        cat: {
+          name: 'Boo',
+          age: 2,
+          enjoys: 'cuddles and belly rubs',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = Cat.first
+      updated_cat_params = {
+        cat: {
+          name: 'Boo',
+          age: '',
+          enjoys: 'cuddles and belly rubs',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      patch "/cats/#{cat.id}", params: updated_cat_params
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(cat['age']).to include "can't be blank"
+    end
+    it 'cannot update a cat without an enjoys' do
+      cat_params = {
+        cat: {
+          name: 'Boo',
+          age: 2,
+          enjoys: 'cuddles and belly rubs',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = Cat.first
+      updated_cat_params = {
+        cat: {
+          name: 'Boo',
+          age: 2,
+          enjoys: '',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      patch "/cats/#{cat.id}", params: updated_cat_params
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(cat['enjoys']).to include "can't be blank"
+    end
+    it 'cannot update a cat without an image' do
+      cat_params = {
+        cat: {
+          name: 'Boo',
+          age: 2,
+          enjoys: 'cuddles and belly rubs',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = Cat.first
+      updated_cat_params = {
+        cat: {
+          name: 'Boo',
+          age: 2,
+          enjoys: 'cuddles and belly rubs',
+          image: ''
+        }
+      }
+      patch "/cats/#{cat.id}", params: updated_cat_params
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(cat['image']).to include "can't be blank"
+    end
+
+    it 'cannot update a cat without an enjoys that is at least 10 characters' do
+      cat_params = {
+        cat: {
+          name: 'Boo',
+          age: 2,
+          enjoys: 'cuddles and belly rubs',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = Cat.first
+      updated_cat_params = {
+        cat: {
+          name: 'Boo',
+          age: 2,
+          enjoys: 'cuddles',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      patch "/cats/#{cat.id}", params: updated_cat_params
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(cat['enjoys']).to include "is too short (minimum is 10 characters)"
     end
   end
 end
